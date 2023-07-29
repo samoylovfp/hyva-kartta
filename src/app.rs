a302c71571fc74d2d022d9891b719909c8a72cdc
Filipp Samoilov <samoylovfp@gmail.com>
2023-07-29 19:39:31 +0300

    Improve UI

--- src/app.rs
+++ src/app.rs
@@ -14,7 +14,7 @@ impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
+            label: "Open Map!".to_owned(),
             value: 2.7,
         }
     }
@@ -65,27 +65,27 @@ impl eframe::App for TemplateApp {
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+            ui.heading("Map options");
 
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
+                ui.label("Search: ");
                 ui.text_edit_singleline(label);
             });
 
             ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
+            if ui.button("Zoom in").clicked() {
                 *value += 1.0;
             }
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
                     ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+                    ui.label("Created by ");
+                    ui.hyperlink_to("Sorseg", "https://github.com/samoylovfp");
                     ui.label(" and ");
                     ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
+                        "Demoth",
+                        "https://demoth.dev",
                     );
                     ui.label(".");
                 });
