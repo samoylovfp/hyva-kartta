da1428f79a78577325ecda13b6ec692c95de388e
Filipp Samoilov <samoylovfp@gmail.com>
2023-08-08 16:20:06 +0300

    Start client api

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,9 +1,12 @@
-use yew::{html, Component};
+use gloo::utils::body;
+use yew::{html, Component, NodeRef};
 use zana::read_zana_data;
 
 struct App {
     x: f32,
     y: f32,
+    canvas: NodeRef,
+    html_size: (u32, u32),
 }
 
 impl Component for App {
@@ -12,13 +15,31 @@ impl Component for App {
     type Properties = ();
 
     fn create(ctx: &yew::Context<Self>) -> Self {
-        App { x: 10.0, y: 10.0 }
+        let body = body();
+        App {
+            x: 10.0,
+            y: 10.0,
+            canvas: NodeRef::default(),
+            html_size: (
+                body.client_width().try_into().unwrap(),
+                body.client_height().try_into().unwrap(),
+            ),
+        }
+    }
+
+    fn update(&mut self, ctx: &yew::Context<Self>, msg: Self::Message) -> bool {
+        true
     }
 
     fn view(&self, ctx: &yew::Context<Self>) -> yew::Html {
+        let (width, height) = self.html_size;
         html! {
             <>
-            <canvas id="map"></canvas>
+            <canvas
+                ref={&self.canvas}
+                width={width.to_string()}
+                height={height.to_string()}
+            ></canvas>
             </>
         }
     }
