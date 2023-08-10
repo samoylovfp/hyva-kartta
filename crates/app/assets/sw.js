37b4dd1d3d52520e4d43812cca2e30557ea90669
Filipp Samoilov <samoylovfp@gmail.com>
2023-08-10 15:37:47 +0300

    Fix filenames for files to cache

--- crates/app/assets/sw.js
+++ crates/app/assets/sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'hyka-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './hyka.js',
+  './hyka_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
